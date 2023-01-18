54d746f352bbfa4bf11a8f867aa2adbd1d0e3f68
Vrukshali <borolevrukshali95@gmail.com>
2023-01-18 13:54:30 +0530

    first commit

---
+++ src/com/example/Test.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(12);
+		al.add(32);
+		al.add(42);
+		System.out.println(al);
+		
+
+	}
+
+}
